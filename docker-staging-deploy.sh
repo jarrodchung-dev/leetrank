b52d04aef075d933a16cd364b2ae7f7c4b4a972a
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-02-17 22:51:06 -0500

    Added ecs task definitions in JSON format and a docker-deploy script to run on build success

---
+++ docker-staging-deploy.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# If the branch is staging and not a pull request, the AWS CLI is configured 
+# and then the deploy_ecs_cluster function is fired which updates the existing
+# Task Definitions found in the JSON files from the ecs directory.
+
+if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ] 
+then
+  if [ "$TRAVIS_BRANCH" == "staging" ] 
+  then
+
+    JQ="jq --raw-output --exit-status" 
+
+    configure_aws_cli() {
+      aws --version
+      aws configure set default.region us-east-1
+      aws configure set default.output json
+      echo "Successfully configured AWS CLI!"
+    }
+
+    register_task_definition() {
+      if revision=$(aws ecs register-task-definition \
+        --cli-input json "$task_definition" | $JQ '.taskDefinition.taskDefinitionArn'
+      );
+      then 
+        echo "Revision: $revision"
+      else
+        echo "Failed to register task definition"
+        return 1
+      fi
+    }
+
+    deploy_ecs_cluster() {
+      # users
+      template="ecs-users-stage-taskdefinition.json"
+      task_template=$(cat "ecs/$template")
+      task_definition=$(printf "$task_template" $AWS_ACCOUNT_ID)
+      echo "New Task Definition: $task_definition"
+      register_task_definition
+
+      template="ecs-client-stage-taskdefinition.json"
+      task_template=$(cat "ecs/$template")
+      task_definition=$(printf "$task_template" $AWS_ACCOUNT_ID)
+      echo "New Task Definition: $task_definition"
+      register_task_definition
+
+      template="ecs-swagger-stage-taskdefinition.json"
+      task_template=$(cat "ecs/$template")
+      task_definition=$(printf "$task_template" $AWS_ACCOUNT_ID)
+      echo "New Task Definition: $task_definition"
+      register_task_definition
+    }
+
+    configure_aws_cli
+    deploy_ecs_cluster
+
+  fi
+fi
