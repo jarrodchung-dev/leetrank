d3ba7ee51eca3120f1c47a6b5f3e6f4b9318e91e
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-02-17 21:03:28 -0500

    Edited error in docker-ecr-push.sh #4

--- docker-ecr-push.sh
+++ docker-ecr-push.sh
@@ -1,23 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
-then 
+then
 
   if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     export DOCKER_ENV=stage
     export REACT_APP_USERS_SERVICE_URL="leetrank-staging-alb-59231334.us-east-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
-    export DOKCER_ENV=prod
+    export DOCKER_ENV=prod
   fi
 
-  if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ] 
-  then 
+  if [ "$TRAVIS_BRANCH" == "staging" ] || \
+     [ "$TRAVIS_BRANCH" == "production" ]
+  then
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
     unzip awscli-bundle.zip
     ./awscli-bundle/install -b ~/bin/aws
     export PATH=~/bin:$PATH
-    aws configure --region 
-    eval $(aws ecr get-login-password)
+    eval $(aws ecr get-login --region us-east-1 --no-include-email)
     export TAG=$TRAVIS_BRANCH
     export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
   fi
@@ -36,9 +36,10 @@ then
       --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
-
+    
     docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
-    docker tag $SWAGGER:$COMMIT $REPO$SWAGGER:$TAG
+    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
     docker push $REPO/$SWAGGER:$TAG
   fi
+
 fi
