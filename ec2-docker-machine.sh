341cd9e7e63f0896673182aa97d65564fcad9ee7
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-02-17 22:25:38 -0500

    Removing ec2-docker-machine.sh

--- ec2-docker-machine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker-machine create \
-    --driver amazonec2 \
-    --amazonec2-access-key $AWS_ACCESS_KEY_ID \
-    --amazonec2-region $AWS_DEFAULT_REGION \
-    --amazonec2-ssh-keypath ~/.ssh \
-    --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY \
-    leetrank-stage
