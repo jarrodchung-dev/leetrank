f61173999aa0a43179733e31771081165115e219
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-02-18 00:19:02 -0500

    Updated docker-staging-deploy.sh and ECS task definitions for users

--- docker-staging-deploy.sh
+++ docker-staging-deploy.sh
@@ -8,8 +8,9 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]
 then
   if [ "$TRAVIS_BRANCH" == "staging" ] 
   then
+
     JQ="jq --raw-output --exit-status" 
-    
+
     configure_aws_cli() {
       aws --version
       aws configure set default.region us-east-1
@@ -18,11 +19,9 @@ then
     }
 
     register_task_definition() {
-      if revision=$(
-        aws ecs register-task-definition \
-        --cli-input json "$task_definition" \
-        | $JQ '.taskDefinition.taskDefinitionArn'
-      )
+      if revision=$(aws ecs register-task-definition \
+        --cli-input json "$task_definition" | $JQ '.taskDefinition.taskDefinitionArn'
+      );
       then 
         echo "Revision: $revision"
       else
@@ -31,47 +30,25 @@ then
       fi
     }
 
-    update_service() {
-      if [[ $(aws ecs update-service \
-        --cluster $cluster \
-        --service $service \
-        --task-definition $revision \
-        | $JQ '.service.taskDefinition')
-        != $revision 
-      ]];
-      then 
-        echo "Error updating service."
-        return 1
-      fi
-    }
-
     deploy_ecs_cluster() {
-
-      cluster="leetrank-staging-cluster"
       # users
-      service="leetrank-users-stage-service"
       template="ecs-users-stage-taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_definition=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "New Task Definition: $task_definition"
       register_task_definition
-      update_service
 
-      service="leetrank-client-stage-service"
       template="ecs-client-stage-taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_definition=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "New Task Definition: $task_definition"
       register_task_definition
-      update_service
 
-      service="leetrank-swagger-stage-service"
       template="ecs-swagger-stage-taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_definition=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "New Task Definition: $task_definition"
       register_task_definition
-      update_service
     }
 
     configure_aws_cli
