ac1e667b8fcdff160d885afcdc4af85ab1e16c61
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-02-17 21:38:48 -0500

    Fixed syntax error in docker-ecr-push script

--- docker-ecr-push.sh
+++ docker-ecr-push.sh
@@ -10,7 +10,8 @@ then
     export DOCKER_ENV=prod
   fi
 
-  if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ] then
+  if [ "$TRAVIS_BRANCH" == "staging" ] || [ "$TRAVIS_BRANCH" == "production" ] 
+  then
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
     unzip awscli-bundle.zip
     ./awscli-bundle/install -b ~/bin/aws
